0fbb2902a240d98834f5953b126fd050a9c1b020
Santiago-Farias <santifa8@gmail.com>
2026-04-14 03:16:08 -0300

    feat: create Track class with basics getters, setters and attributes feat: add submenues for the option 1 (Add track) and submenu for pick genere

--- src/main/main.java
+++ src/main/main.java
@@ -1,15 +1,25 @@
 package main;
 
+import java.util.ArrayList;
 import java.util.Scanner;
+import model.Track;
 
 public class main {
 
 
     public static void main(String[] args) {
         
-        int opcion = 100;
+        int mainOption = 100;
+        int genereOption = 100;
+        String artistTemp = "";
+        String nameTemp = "";
+        String genereTemp = "";
+        int bpmTemp = 0;
+        int secDurationTemp = 0;
         
         Scanner scanner = new Scanner(System.in);
+        
+        ArrayList<Track> playList = new ArrayList<>();
         
         do {
             System.out.println("=== NEURORAVE v0.1 ===");
@@ -20,13 +30,47 @@ public class main {
             System.out.println("0. Exit");
             
             System.out.print("Enter your choice: ");
-            opcion = scanner.nextInt();
+            mainOption = scanner.nextInt();
             
-            if (opcion != 0) {
+            if (mainOption == 1) {
+                System.out.println("");
+                System.out.println("Enter the artist: ");
+                artistTemp = scanner.nextLine();
+                System.out.println("");
+                System.out.println("Enter track name: ");
+                nameTemp = scanner.nextLine();
+                System.out.println("");
+                
+                // Genere menu loop
+                do {
+                    System.out.println("1. Breakcore");
+                    System.out.println("2. Dubstep");
+                    System.out.println("3. Vocaloid");
+                    System.out.println("Select the genere by entering a number: ");
+                    genereOption = scanner.nextInt();
+                } while (genereOption >= 1 && genereOption <= 3);
+                System.out.println("");
+                System.out.println("Enter track bpm: ");
+                bpmTemp = scanner.nextInt();
+                System.out.println("");
+                System.out.println("Enter track duration in seconds: ");
+                secDurationTemp = scanner.nextInt();
+                
+                // ideally, have a array with generes and validation in input
+                switch (genereOption) {
+                    case 1 -> genereTemp = "Breakcore";
+                    case 2 -> genereTemp = "Dubstep";
+                    case 3 -> genereTemp = "Vocaloid";
+                    default -> {
+                    }
+                }
+            }
+            
+            if (mainOption != 0) {
                 System.out.println("\nWork in progress..\n");
             }
             
-        } while (opcion != 0);
+        } while (mainOption != 0);
         
         
         
--- src/model/Track.java
+++ src/model/Track.java
@@ -1,5 +1,72 @@
 package model;
 
 public class Track {
+    private String artist = "";
+    private String name = "";
+    private String genere = "";
+    private int bpm = 0;
+    private int secDuration = 0;
     
+    // Constructor
+    public Track(String artistTrack, String nameTrack, String genereTrack, int bpmTrack, int secDuracionTrack) {
+        this.artist = artistTrack;
+        this.name = nameTrack;
+        this.genere = genereTrack;
+        this.bpm = bpmTrack;
+        this.secDuration = secDuracionTrack;
+    }
+    
+    // Metodos principales
+    public void reproducir() {
+        System.out.println("> Reproduciendo " + getArtist() + " - " + getName() + " [" + getMinutesDuration() + "]");
+    }
+    
+    public String getMinutesDuration() {
+        int minutos = secDuration / 60;
+        int segundos = secDuration % 60;
+        
+        return minutos + ":" + (segundos < 10 ? "0" : "") + segundos;
+    }
+    
+    // Getters
+    public String getArtist() {
+        return this.artist;
+    }
+    
+    public String getName() {
+        return this.name;
+    }
+    
+    public String getGenere() {
+        return this.genere;
+    }
+    
+    public int getBpm() {
+        return this.bpm;
+    }
+    
+    public int getSecDuration() {
+        return this.secDuration;
+    }
+    
+    // Setters
+    public void setArtist(String newArtist) {
+        this.artist = newArtist;
+    }
+    
+    public void setName(String newName) {
+        this.name = newName;
+    }
+    
+    public void setGenere(String newGenere) {
+        this.genere = newGenere;
+    }
+    
+    public void setBpm(int newBpm) {
+        this.bpm = newBpm;
+    }
+    
+    public void setSecDuration(int newSecDuration) {
+        this.secDuration = newSecDuration;
+    }
 }
